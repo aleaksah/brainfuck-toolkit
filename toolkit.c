73fc3474bee1eeb76c0c0d098700401c965ba352
aleaksah <aleaksah@gmail.com>
2017-09-12 16:22:27 +0300

    Initial commit

---
+++ toolkit.c
@@ -0,0 +1,9 @@
+#include "toolkit.h"
+
+
+int main(int argc, char **argv)
+{
+
+
+
+}
---
+++ toolkit.h
@@ -0,0 +1,10 @@
+#ifndef TOOLKIT_H
+#define TOOLKIT_H
+
+#include "compiler.h"
+#include "debugger.h"
+#include "interpreter.h"
+
+
+
+#endif
